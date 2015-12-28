eaba8b31cff3b3168b236cc127e92813800fca6f
NikichXP <nikichx2@gmail.com>
2015-12-28 22:23:59 +0200

    Refactor mockito
    Done

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -3,18 +3,21 @@ import static org.junit.Assert.*;
 import java.util.Iterator;
 
 import dao.UserDAO;
-import dao.entity.UserEntity;
+import dao.entity.BusEntity;
 import org.junit.Test;
 import rest.UserRest;
 
 public class ProjectTest {
+	
 	@Test
-	public void testCalA() throws Exception {
-		assertEquals(4, 2+2);
+	public void testBusEntity() throws Exception {
+		BusEntity be1 = new BusEntity(1, "Icarus", 15);
+		BusEntity be2 = new BusEntity(2, "Brabus", 2);
+		assertNotEquals(be1, be2);
 	}
 
 	@Test
-	public void usersNotEqual() {
+	public void usersEqual() {
 		UserRest ur = mock(UserRest.class);
 		when(ur.validate("truetoken")).thenReturn("true");
 		assertEquals("true", ur.validate("truetoken"));
