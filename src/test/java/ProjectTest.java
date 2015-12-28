7ecc76cea40c78805efe891c7f2a6d7c69f0ca21
NikichXP <nikichx2@gmail.com>
2015-12-28 22:13:08 +0200

    Mockito Demo
    Kalyzhny Sasha

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import dao.UserDAO;
 import dao.entity.UserEntity;
 import org.junit.Test;
+import rest.RouteRest;
 import rest.UserRest;
 
 public class ProjectTest {
@@ -14,10 +15,25 @@ public class ProjectTest {
 	}
 
 	@Test
-	public void usersNotEqual() {
+	public void usersEqual() {
 		UserRest ur = mock(UserRest.class);
 		when(ur.validate("truetoken")).thenReturn("true");
 		assertEquals("true", ur.validate("truetoken"));
+	}
+
+	@Test
+	public void testEntity() throws Exception {
+		UserEntity userEnt1 = new UserEntity(1, "admin", "pass", "name", "surname", "car", "no", 1, "someToken");
+		UserEntity userEnt2 = new UserEntity(1, "admin", "pass", "name", "surname", "car", "no", 1);
+		userEnt2.setToken("someToken");
+		assertEquals(userEnt1, userEnt2);
+	}
+
+	@Test
+	public void usersNotEqual() {
+		RouteRest rest = mock(RouteRest.class);
+		when(rest.addClient("someToken", "1")).thenReturn("Success!");
+		assertEquals("Success!", rest.addClient("someToken", "1"));
 	}
 
 }
