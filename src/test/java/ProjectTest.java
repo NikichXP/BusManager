e4eaec7490e24369f92176d0189684007fdfd288
NikichXP <nikichx2@gmail.com>
2015-12-28 22:20:20 +0200

    Revert "Mockito Demo"
    This reverts commit 7ecc76cea40c78805efe891c7f2a6d7c69f0ca21.

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import dao.UserDAO;
 import dao.entity.UserEntity;
 import org.junit.Test;
-import rest.RouteRest;
 import rest.UserRest;
 
 public class ProjectTest {
@@ -15,25 +14,10 @@ public class ProjectTest {
 	}
 
 	@Test
-	public void usersEqual() {
+	public void usersNotEqual() {
 		UserRest ur = mock(UserRest.class);
 		when(ur.validate("truetoken")).thenReturn("true");
 		assertEquals("true", ur.validate("truetoken"));
-	}
-
-	@Test
-	public void testEntity() throws Exception {
-		UserEntity userEnt1 = new UserEntity(1, "admin", "pass", "name", "surname", "car", "no", 1, "someToken");
-		UserEntity userEnt2 = new UserEntity(1, "admin", "pass", "name", "surname", "car", "no", 1);
-		userEnt2.setToken("someToken");
-		assertEquals(userEnt1, userEnt2);
-	}
-
-	@Test
-	public void usersNotEqual() {
-		RouteRest rest = mock(RouteRest.class);
-		when(rest.addClient("someToken", "1")).thenReturn("Success!");
-		assertEquals("Success!", rest.addClient("someToken", "1"));
 	}
 
 }
